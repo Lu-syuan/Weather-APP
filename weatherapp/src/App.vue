5d4626780d107da86557cf03d37592898fb9f8c5
Lu-syuan <as6552783@gmail.com>
2022-10-28 11:23:50 +0800

    add style

--- weatherapp/src/App.vue
+++ weatherapp/src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container">
+      <div class="search-box">
+        <input class="search-bar" type="text" placeholder="Search..." />
+      </div>
+
+      <div class="weather-wrapper">
+        <div class="location-box">
+          <div class="location">taichung</div>
+          <div class="date">Octorber 8th 2022</div>
+        </div>
+      </div>
+
+      <div class="weather-box">
+        <div class="temperature">26°C</div>
+        <div class="weather">clouds</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +26,97 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  height: 100vh;
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: 100% 100%;
+}
+
+.container {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+  outline: none;
+  border: none;
+  border-radius: 0 16px 0 16px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0 16px 0;
+}
+
+.location-box .location {
+  font-size: 32px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  font-size: 20px;
+  color: #fff;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  font-size: 100px;
+  font-weight: 900;
+  color: #fff;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 40px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
